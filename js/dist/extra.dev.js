1e7d8a0693271e907f4465c15f857a70e8b7dc3e
Alejandro Del Río Salas <adrs.desarrollo@gmail.com>
2021-02-21 19:25:55 -0300

    Modificado el formulario de contactos// modificada completamente la pagina index// el nav pasa a hamburguesa para todos los tamaños// modficaciones en extrajs

---
+++ js/dist/extra.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Modal
+var myModal = document.getElementById('myModal');
+var myInput = document.getElementById('myInput');
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus();
+}); // Modal
+// Side menu
+
+function toggleMenu() {
+  var menuToggle = document.querySelector('.toggle');
+  var sidebar = document.querySelector('.sidebar');
+  menuToggle.classList.toggle('active');
+  sidebar.classList.toggle('active');
+} // Side menu
--- js/extra.js
+++ js/extra.js
@@ -1,5 +1,19 @@
+
+// Modal
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+// Modal
+
+// Side menu
+
+function toggleMenu(){
+  const menuToggle = document.querySelector('.toggle');
+  const sidebar = document.querySelector('.sidebar');
+  menuToggle.classList.toggle('active')
+  sidebar.classList.toggle('active')
+}
+
+// Side menu
